f393d30afaddc48792c49d7ce64322765bdd1cea
Aidai <you@ubaidaidillaevagmail.com>
2021-09-28 21:34:32 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# список книг {назв.книги: автор}
+# books = {'Financial': 'Theodor Draiser',
+# 'Rich dad, poor dad': 'Robert Kiyosaki',
+# 'Think and get rich': 'Napoleon Hill'}
+
+# список читателей {имя: {назв.книги: автор}}
+# {'Alex': {'Financial': 'Theodor Draiser'}}
+
+# функция, которая выводит информацию о книгах
+# которые есть и о читателях
+# На данный момент список читателей такой:
+# {'Alex': {'Financial': 'Theodor Draiser'}}
+# Список книг такой:
+# {'Rich dad, poor dad': 'Robert Kiyosaki', 'Think and get rich': 'Napoleon Hill'}
+
+# функцмя, которая регистрирует читателя и выдает книгу
+# Какую книгу вы хотите получить? Выберите из
+# {'Financial': 'Theodor Draiser', 'Rich dad, poor dad': 'Robert Kiyosaki', 'Think and get rich': 'Napoleon Hill'}
+# Введите только название: Financial
+# Введите ваше имя: Alex
+# Ваша заявка успешно оформлена!
+
+# функция, которая возвращает книгу
+# Вы хотите взять книгу либо вернуть? Введите "взять" или "вернуть": вернуть
+# Введите имя: Alex
+# Вы хотите вернуть: {'Financial': 'Theodor Draiser'}? да/нет: да
+# Книга успешно возвращена!
+
+# функция менеджер, которая распределяет
+# вызов других функций
+
+
+
+
+
+
